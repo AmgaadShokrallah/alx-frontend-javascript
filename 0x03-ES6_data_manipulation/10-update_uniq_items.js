aff3dcf6555af48e2bd23a46793f56178ac2d5a3
AmgaadShokrallah <amgaad@example.com>
2024-07-02 17:13:48 +0000

    commit

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,8 +3,8 @@ export default function updateUniqueItems(map) {
     throw Error('Cannot process');
   }
   for (const elements of map) {
-    if (e[1] === 1) {
-      map.set(e[0], 100);
+    if (element[1] === 1) {
+      map.set(element[0], 100);
     }
   }
   return map;
